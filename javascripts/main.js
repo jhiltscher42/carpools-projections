4630305ab602a0f4703a94e0fab9dedddc0dcd0c
Justin Hiltscher <j.hiltscher@gmail.com>
2015-05-14 13:43:34 -0500

    Posed the first iteration of the Question

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+/*
+Problem we're interested in: A Community of some number C of people wants to use a pool of cars of size X for typical usage patterns.
+Each car has a cost-per-mile
+Each car has a finite-but-unknown lifespan before it needs replacement.
+Members of the community make plans for outings of various lengths, 
+  and have different tolerances for scheduling: whenever, scheduled, emergency.
+Failure to deliver on emergencies should be considered a critical failure of the system.
+Failure to deliver on scheduled should be considered a lesser failure, and should be measured and reported in mean-minutes-late.
+*/
+
+/*
+How to model this?
+*/
